521e9f7ef414c7b9c233438159fc5606530ec0ee
Mahziyar Gilanpour <mgillanpour79@gmail.com>
2022-03-20 11:44:20 +0330

    darkmode image fixed

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,14 +4,13 @@ import logo from "../assets/images/Logo.png";
 import { useSelector } from "react-redux";
 import toggleOn from "../assets/images/toggle-on.png";
 import toggleOff from "../assets/images/toggle-off.png";
-import { useDarkMode,useDarkModeActions } from "../Context/DarkProvider";
+import { useDarkMode, useDarkModeActions } from "../Context/DarkProvider";
 
 function AppBar({ title }) {
   const productData = useSelector((state) => state.productReducer.cart);
 
   const darkMode = useDarkMode();
   const darkModeDispatch = useDarkModeActions();
-
 
   return (
     <>
@@ -25,42 +24,44 @@ function AppBar({ title }) {
         <div className="font-bold text-slate-800 dark:text-white text-lg">
           {title}
         </div>
-        <span onClick={() => darkModeDispatch(!darkMode)}>
-          {darkMode ? (
-            <img
-              className="w-10 ml-4 cursor-pointer"
-              src={toggleOn}
-              alt="toggleon"
-            />
-          ) : (
-            <img
-              className="w-10 ml-4 cursor-pointer"
-              src={toggleOff}
-              alt="toggleoff"
-            />
-          )}
-        </span>
-        <Link to={"/cart"}>
-          <div className="bg-white p-2 rounded-md shadow-lg relative">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        <div className="flex gap-x-5 items-center">
+          <span onClick={() => darkModeDispatch(!darkMode)}>
+            {darkMode ? (
+              <img
+                className="w-10 cursor-pointer"
+                src={toggleOn}
+                alt="toggleon"
               />
-            </svg>
-            <span className="bg-red-500 w-4 h-4 rounded-full absolute -top-2 -right-1 flex items-center justify-center text-white p-2 text-xs">
-              {productData.length}
-            </span>
-          </div>
-        </Link>
+            ) : (
+              <img
+                className="w-10 cursor-pointer"
+                src={toggleOff}
+                alt="toggleoff"
+              />
+            )}
+          </span>
+          <Link to={"/cart"}>
+            <div className="bg-white p-2 rounded-md shadow-lg relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="bg-red-500 w-4 h-4 rounded-full absolute -top-2 -right-1 flex items-center justify-center text-white p-2 text-xs">
+                {productData.length}
+              </span>
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
